ac121d0a0e73a9db82099d1641f592151791050e
Junwenlaw <97320660+Junwenlaw@users.noreply.github.com>
2025-07-16 03:47:39 -0400

    Update cluster_sequential.jl

--- src/algorithms/cluster_sequential.jl
+++ src/algorithms/cluster_sequential.jl
@@ -66,7 +66,7 @@ function cluster_sequential(myTDRsetup::Dict, ClusteringInputDF::DataFrame, NClu
         x -> permutedims(x, (3,2,1)),   # (T, C, B)
         conv_layer,
         x -> leakyrelu.(x),
-        x -> flatten(permutedims(x, (3,2,1))),  # (B, F×T')
+        x -> reshape(permutedims(x, (3,2,1)), size(x, 3), :),  # (B, F*T′)
         dense_layer                          # (B, latent_dim)
     )
 
